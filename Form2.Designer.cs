4e9b7825ade8fb6b542a28e563a1ba8d0fe249dd
RAFAEL MORAES <rafael.moraes97@etec.sp.gov.br>
2024-03-11 15:24:01 -0300

    atualizações na tela de novo cadastro

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -36,26 +36,28 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // txt_user
             // 
-            this.txt_user.Location = new System.Drawing.Point(380, 106);
+            this.txt_user.Location = new System.Drawing.Point(145, 84);
             this.txt_user.Name = "txt_user";
-            this.txt_user.Size = new System.Drawing.Size(134, 22);
+            this.txt_user.Size = new System.Drawing.Size(155, 22);
             this.txt_user.TabIndex = 0;
             // 
             // txt_password
             // 
-            this.txt_password.Location = new System.Drawing.Point(380, 167);
+            this.txt_password.Location = new System.Drawing.Point(145, 146);
             this.txt_password.Name = "txt_password";
-            this.txt_password.Size = new System.Drawing.Size(134, 22);
+            this.txt_password.Size = new System.Drawing.Size(155, 22);
             this.txt_password.TabIndex = 1;
             // 
             // btn_salvar
             // 
             this.btn_salvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_salvar.Location = new System.Drawing.Point(357, 259);
+            this.btn_salvar.Location = new System.Drawing.Point(144, 238);
             this.btn_salvar.Name = "btn_salvar";
             this.btn_salvar.Size = new System.Drawing.Size(75, 23);
             this.btn_salvar.TabIndex = 2;
@@ -64,16 +66,16 @@
             // 
             // txt_Email
             // 
-            this.txt_Email.Location = new System.Drawing.Point(380, 219);
+            this.txt_Email.Location = new System.Drawing.Point(145, 198);
             this.txt_Email.Name = "txt_Email";
-            this.txt_Email.Size = new System.Drawing.Size(134, 22);
+            this.txt_Email.Size = new System.Drawing.Size(155, 22);
             this.txt_Email.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(291, 106);
+            this.label1.Location = new System.Drawing.Point(72, 84);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 18);
             this.label1.TabIndex = 4;
@@ -83,7 +85,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(303, 167);
+            this.label2.Location = new System.Drawing.Point(84, 146);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 18);
             this.label2.TabIndex = 5;
@@ -93,7 +95,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(308, 219);
+            this.label3.Location = new System.Drawing.Point(89, 199);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(50, 18);
             this.label3.TabIndex = 6;
@@ -102,7 +104,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(451, 259);
+            this.button2.Location = new System.Drawing.Point(225, 238);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 7;
@@ -110,11 +112,22 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(378, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(391, 323);
+            this.dataGridView1.TabIndex = 8;
+            // 
             // txt_telaCadastro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -125,6 +138,7 @@
             this.Controls.Add(this.txt_user);
             this.Name = "txt_telaCadastro";
             this.Text = "Novo Cadastro";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,5 +154,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
