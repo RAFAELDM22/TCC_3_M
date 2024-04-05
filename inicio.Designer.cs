e9bb0312eba8dbeee5cf00c532a8d16d32d4544e
WALLACE SANTOS <wallace.santos74@etec.sp.gov.br>
2024-04-04 21:59:38 -0300

    Linkagem da tela inicio com a tela excluirDispositivo

--- inicio.Designer.cs
+++ inicio.Designer.cs
@@ -30,24 +30,38 @@ namespace TCC_3_M
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(inicio));
-            this.btnEstoque = new System.Windows.Forms.Button();
-            this.btnCadastrarDispositivo = new System.Windows.Forms.Button();
-            this.btnExcluir = new System.Windows.Forms.Button();
             this.btnCadastrarUsuario = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnCadastrarDispositivo = new System.Windows.Forms.Button();
+            this.btnEstoque = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnEstoque
+            // btnCadastrarUsuario
             // 
-            this.btnEstoque.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEstoque.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEstoque.Image = ((System.Drawing.Image)(resources.GetObject("btnEstoque.Image")));
-            this.btnEstoque.Location = new System.Drawing.Point(239, 110);
-            this.btnEstoque.Name = "btnEstoque";
-            this.btnEstoque.Size = new System.Drawing.Size(136, 104);
-            this.btnEstoque.TabIndex = 0;
-            this.btnEstoque.Text = "Estoque";
-            this.btnEstoque.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnEstoque.UseVisualStyleBackColor = true;
+            this.btnCadastrarUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrarUsuario.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrarUsuario.Image = ((System.Drawing.Image)(resources.GetObject("btnCadastrarUsuario.Image")));
+            this.btnCadastrarUsuario.Location = new System.Drawing.Point(239, 236);
+            this.btnCadastrarUsuario.Name = "btnCadastrarUsuario";
+            this.btnCadastrarUsuario.Size = new System.Drawing.Size(136, 104);
+            this.btnCadastrarUsuario.TabIndex = 3;
+            this.btnCadastrarUsuario.Text = "Cadastrar Usuário";
+            this.btnCadastrarUsuario.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnCadastrarUsuario.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcluir.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Image = ((System.Drawing.Image)(resources.GetObject("btnExcluir.Image")));
+            this.btnExcluir.Location = new System.Drawing.Point(417, 236);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(136, 104);
+            this.btnExcluir.TabIndex = 2;
+            this.btnExcluir.Text = "Deletar";
+            this.btnExcluir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
             // 
             // btnCadastrarDispositivo
             // 
@@ -63,31 +77,18 @@ namespace TCC_3_M
             this.btnCadastrarDispositivo.UseVisualStyleBackColor = true;
             this.btnCadastrarDispositivo.Click += new System.EventHandler(this.btnCadastrarDispositivo_Click);
             // 
-            // btnExcluir
+            // btnEstoque
             // 
-            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExcluir.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExcluir.Image = ((System.Drawing.Image)(resources.GetObject("btnExcluir.Image")));
-            this.btnExcluir.Location = new System.Drawing.Point(417, 236);
-            this.btnExcluir.Name = "btnExcluir";
-            this.btnExcluir.Size = new System.Drawing.Size(136, 104);
-            this.btnExcluir.TabIndex = 2;
-            this.btnExcluir.Text = "Deletar";
-            this.btnExcluir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnExcluir.UseVisualStyleBackColor = true;
-            // 
-            // btnCadastrarUsuario
-            // 
-            this.btnCadastrarUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCadastrarUsuario.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCadastrarUsuario.Image = ((System.Drawing.Image)(resources.GetObject("btnCadastrarUsuario.Image")));
-            this.btnCadastrarUsuario.Location = new System.Drawing.Point(239, 236);
-            this.btnCadastrarUsuario.Name = "btnCadastrarUsuario";
-            this.btnCadastrarUsuario.Size = new System.Drawing.Size(136, 104);
-            this.btnCadastrarUsuario.TabIndex = 3;
-            this.btnCadastrarUsuario.Text = "Cadastrar Usuário";
-            this.btnCadastrarUsuario.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnCadastrarUsuario.UseVisualStyleBackColor = true;
+            this.btnEstoque.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEstoque.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEstoque.Image = ((System.Drawing.Image)(resources.GetObject("btnEstoque.Image")));
+            this.btnEstoque.Location = new System.Drawing.Point(239, 110);
+            this.btnEstoque.Name = "btnEstoque";
+            this.btnEstoque.Size = new System.Drawing.Size(136, 104);
+            this.btnEstoque.TabIndex = 0;
+            this.btnEstoque.Text = "Estoque";
+            this.btnEstoque.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnEstoque.UseVisualStyleBackColor = true;
             // 
             // inicio
             // 
