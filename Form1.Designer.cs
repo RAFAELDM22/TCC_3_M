2bb0c473bbdd819b0ed60bd69104a8c63c83cf8b
RAFAELDM22 <rafael.moraes97@etec.sp.gov.br>
2024-04-04 22:09:11 -0300

    teste

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,57 +34,58 @@
             this.label2 = new System.Windows.Forms.Label();
             this.btn_entrar = new System.Windows.Forms.Button();
             this.btn_cadastro = new System.Windows.Forms.Button();
-            this.txt_email = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txt_usuario
             // 
             this.txt_usuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txt_usuario.Location = new System.Drawing.Point(60, 61);
+            this.txt_usuario.Location = new System.Drawing.Point(188, 112);
             this.txt_usuario.Margin = new System.Windows.Forms.Padding(2);
             this.txt_usuario.Name = "txt_usuario";
-            this.txt_usuario.Size = new System.Drawing.Size(218, 13);
+            this.txt_usuario.Size = new System.Drawing.Size(114, 13);
             this.txt_usuario.TabIndex = 0;
             // 
             // txt_senha
             // 
             this.txt_senha.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txt_senha.Location = new System.Drawing.Point(60, 109);
+            this.txt_senha.Location = new System.Drawing.Point(49, 112);
             this.txt_senha.Margin = new System.Windows.Forms.Padding(2);
             this.txt_senha.Name = "txt_senha";
-            this.txt_senha.Size = new System.Drawing.Size(218, 13);
+            this.txt_senha.Size = new System.Drawing.Size(114, 13);
             this.txt_senha.TabIndex = 1;
             // 
             // lblUser1
             // 
             this.lblUser1.AutoSize = true;
-            this.lblUser1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUser1.Location = new System.Drawing.Point(57, 44);
+            this.lblUser1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblUser1.Location = new System.Drawing.Point(184, 86);
             this.lblUser1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUser1.Name = "lblUser1";
-            this.lblUser1.Size = new System.Drawing.Size(61, 15);
+            this.lblUser1.Size = new System.Drawing.Size(87, 24);
             this.lblUser1.TabIndex = 2;
             this.lblUser1.Text = "Usuario:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(57, 92);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(45, 86);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.Size = new System.Drawing.Size(76, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "Senha:";
             // 
             // btn_entrar
             // 
-            this.btn_entrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_entrar.Location = new System.Drawing.Point(60, 206);
+            this.btn_entrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_entrar.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btn_entrar.Location = new System.Drawing.Point(92, 164);
             this.btn_entrar.Margin = new System.Windows.Forms.Padding(2);
             this.btn_entrar.Name = "btn_entrar";
-            this.btn_entrar.Size = new System.Drawing.Size(67, 31);
+            this.btn_entrar.Size = new System.Drawing.Size(78, 31);
             this.btn_entrar.TabIndex = 4;
             this.btn_entrar.Text = "Entrar";
             this.btn_entrar.UseVisualStyleBackColor = true;
@@ -92,44 +93,23 @@
             // 
             // btn_cadastro
             // 
-            this.btn_cadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_cadastro.Location = new System.Drawing.Point(172, 206);
+            this.btn_cadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cadastro.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btn_cadastro.Location = new System.Drawing.Point(174, 164);
             this.btn_cadastro.Margin = new System.Windows.Forms.Padding(2);
             this.btn_cadastro.Name = "btn_cadastro";
-            this.btn_cadastro.Size = new System.Drawing.Size(87, 31);
+            this.btn_cadastro.Size = new System.Drawing.Size(93, 31);
             this.btn_cadastro.TabIndex = 5;
             this.btn_cadastro.Text = "Cadastrar";
             this.btn_cadastro.UseVisualStyleBackColor = true;
             this.btn_cadastro.Click += new System.EventHandler(this.btn_cadastro_Click);
             // 
-            // txt_email
-            // 
-            this.txt_email.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txt_email.Location = new System.Drawing.Point(60, 164);
-            this.txt_email.Margin = new System.Windows.Forms.Padding(2);
-            this.txt_email.Name = "txt_email";
-            this.txt_email.Size = new System.Drawing.Size(218, 13);
-            this.txt_email.TabIndex = 6;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(57, 147);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 15);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Email:";
-            // 
             // login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(355, 291);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txt_email);
             this.Controls.Add(this.btn_cadastro);
             this.Controls.Add(this.btn_entrar);
             this.Controls.Add(this.label2);
@@ -153,8 +133,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btn_entrar;
         private System.Windows.Forms.Button btn_cadastro;
-        private System.Windows.Forms.TextBox txt_email;
-        private System.Windows.Forms.Label label3;
     }
 }
 
--- inicio.Designer.cs
+++ inicio.Designer.cs
@@ -38,61 +38,70 @@ namespace TCC_3_M
             // 
             // btnEstoque
             // 
+            this.btnEstoque.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
             this.btnEstoque.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnEstoque.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEstoque.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnEstoque.Image = ((System.Drawing.Image)(resources.GetObject("btnEstoque.Image")));
-            this.btnEstoque.Location = new System.Drawing.Point(239, 110);
+            this.btnEstoque.Location = new System.Drawing.Point(108, 107);
             this.btnEstoque.Name = "btnEstoque";
             this.btnEstoque.Size = new System.Drawing.Size(136, 104);
             this.btnEstoque.TabIndex = 0;
             this.btnEstoque.Text = "Estoque";
             this.btnEstoque.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnEstoque.UseVisualStyleBackColor = true;
+            this.btnEstoque.UseVisualStyleBackColor = false;
             // 
             // btnCadastrarDispositivo
             // 
+            this.btnCadastrarDispositivo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
             this.btnCadastrarDispositivo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCadastrarDispositivo.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrarDispositivo.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrarDispositivo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnCadastrarDispositivo.Image = ((System.Drawing.Image)(resources.GetObject("btnCadastrarDispositivo.Image")));
-            this.btnCadastrarDispositivo.Location = new System.Drawing.Point(417, 110);
+            this.btnCadastrarDispositivo.Location = new System.Drawing.Point(286, 107);
             this.btnCadastrarDispositivo.Name = "btnCadastrarDispositivo";
             this.btnCadastrarDispositivo.Size = new System.Drawing.Size(136, 104);
             this.btnCadastrarDispositivo.TabIndex = 1;
             this.btnCadastrarDispositivo.Text = "Cadastrar Periférico";
             this.btnCadastrarDispositivo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnCadastrarDispositivo.UseVisualStyleBackColor = true;
+            this.btnCadastrarDispositivo.UseVisualStyleBackColor = false;
             this.btnCadastrarDispositivo.Click += new System.EventHandler(this.btnCadastrarDispositivo_Click);
             // 
             // btnExcluir
             // 
+            this.btnExcluir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
             this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnExcluir.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnExcluir.Image = ((System.Drawing.Image)(resources.GetObject("btnExcluir.Image")));
-            this.btnExcluir.Location = new System.Drawing.Point(417, 236);
+            this.btnExcluir.Location = new System.Drawing.Point(286, 233);
             this.btnExcluir.Name = "btnExcluir";
             this.btnExcluir.Size = new System.Drawing.Size(136, 104);
             this.btnExcluir.TabIndex = 2;
             this.btnExcluir.Text = "Deletar";
             this.btnExcluir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.UseVisualStyleBackColor = false;
             // 
             // btnCadastrarUsuario
             // 
+            this.btnCadastrarUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
             this.btnCadastrarUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCadastrarUsuario.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrarUsuario.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnCadastrarUsuario.Image = ((System.Drawing.Image)(resources.GetObject("btnCadastrarUsuario.Image")));
-            this.btnCadastrarUsuario.Location = new System.Drawing.Point(239, 236);
+            this.btnCadastrarUsuario.Location = new System.Drawing.Point(108, 233);
             this.btnCadastrarUsuario.Name = "btnCadastrarUsuario";
             this.btnCadastrarUsuario.Size = new System.Drawing.Size(136, 104);
             this.btnCadastrarUsuario.TabIndex = 3;
             this.btnCadastrarUsuario.Text = "Cadastrar Usuário";
             this.btnCadastrarUsuario.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnCadastrarUsuario.UseVisualStyleBackColor = true;
+            this.btnCadastrarUsuario.UseVisualStyleBackColor = false;
             // 
             // inicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnCadastrarUsuario);
             this.Controls.Add(this.btnExcluir);
